73c9168de7cffa04377b444927843eadfb0355ea
Hugo Gonzalez <hugo11gonzalez@gmail.com>
2023-08-09 13:57:45 -0300

    2do commit react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header className="App-header">
+      <h1>Soy una Nav</h1>
+    </header>
+    <main>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque facilis cum sed sunt. Pariatur optio modi sint in officia voluptas minus, cupiditate repellendus cumque molestiae, mollitia nesciunt aspernatur autem ipsam.</p>
+    </main>
+    <footer>
+    <p>Todos los derechos reservados @2023 </p>
+    </footer>
     </>
   )
 }
