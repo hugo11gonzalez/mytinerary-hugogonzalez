45ea8a36215daf5941edfd71014fd8a73890a9d3
Hugo Gonzalez <hugo11gonzalez@gmail.com>
2023-08-22 00:07:33 -0300

    4to commit MainLayout continuacion config y tailwindcss

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainLayout = ({children}) => {
+  return (
+    <div className='w-full h-12 bg-slate-500'>
+      <header className='w-100 h-100 flex justify-center items-center bg-lime-900'>
+
+      </header>
+
+    </div>
+  )
+}
+
+export default MainLayout
